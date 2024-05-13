e682e1f384f5aacf73e25c04d27de560550bd5b6
alexwlad3885 <alexwlad3885@yandex.ru>
2024-05-13 22:15:28 +0300

    commit_1

---
+++ Modul_2/homework_7.py
@@ -0,0 +1,10 @@
+list_car = ["BMV", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(list_car)):
+    print('Я езжу на автомобиле марки ', '"', list_car[i], '"')
+cars_count = 0
+for i in range(len(list_car)):
+    if i > 0:
+        cars_count += 10
+        print(str(i), str(cars_count))
+    else:
+        print(str(i), str(cars_count))
