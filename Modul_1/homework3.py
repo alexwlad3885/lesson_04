095903223ae74fce92e226a10f10813066dea487
alexwlad3885 <alexwlad3885@yandex.ru>
2024-07-16 15:03:01 +0300

    Практическая работа по уроку "Динамическая типизация"

---
+++ Modul_1/homework3.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = "Alex"
+print("Name: " + name)
+age = 58
+print("Age: " + str(age))
+age = age + 1
+print("New Age: " + str(age))
+is_student = True
+print("Is Student: " + str(is_student))
