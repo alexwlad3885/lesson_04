99a355f1893055f43dddf3dd32bca5f6b02906bf
alexwlad3885 <alexwlad3885@yandex.ru>
2024-07-16 14:44:38 +0300

    Практическое задание по теме "Переменные."

---
+++ Modul_1/homework2.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "Переменные."
+homework = 12
+hours_spent = 1.5
+name = "Python"
+one_task = hours_spent / homework
+print(f'Курс: {name}, всего задач: {homework}, затрачено часов: {hours_spent},'
+      f'среднее время выполнения {one_task} часа')
